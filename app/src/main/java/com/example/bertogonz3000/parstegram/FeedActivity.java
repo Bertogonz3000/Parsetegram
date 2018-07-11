adb126e4c64b99bf4b35cc852e35c1e452d2b536
Heriberto Gonzalez <bertogonz3000@fb.com>
2018-07-11 11:42:35 -0700

    Can now use create post button in feed to return to createPost page

--- app/src/main/java/com/example/bertogonz3000/parstegram/FeedActivity.java
+++ app/src/main/java/com/example/bertogonz3000/parstegram/FeedActivity.java
@@ -1,13 +1,27 @@
 package com.example.bertogonz3000.parstegram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FeedActivity extends AppCompatActivity {
+
+    private Button createPostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        createPostButton = (Button) findViewById(R.id.createPostButton);
     }
+
+    public void viewCreatePost(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+
 }
