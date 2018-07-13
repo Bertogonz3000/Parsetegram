2e2b61d1831412c226b4c75626bc2dd54a03c863
Heriberto Gonzalez <bertogonz3000@fb.com>
2018-07-12 17:05:26 -0700

    some login UI to match real insta:

--- app/src/main/java/com/example/bertogonz3000/parstegram/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/bertogonz3000/parstegram/Fragments/ProfileFragment.java
@@ -1,18 +1,24 @@
 package com.example.bertogonz3000.parstegram.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.example.bertogonz3000.parstegram.LoginActivity;
 import com.example.bertogonz3000.parstegram.R;
+import com.parse.ParseUser;
 
 
 public class ProfileFragment extends Fragment {
+
+    Button logoutButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
@@ -21,5 +27,15 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
+        logoutButton = (Button) view.findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                Intent i = new Intent(view.getContext(), LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
